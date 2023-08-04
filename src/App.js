8c4cdaecc092c5b470c21a5e39381e75ede2a8d7
daniel endaylalu <endaylaludaniel@gmail.com>
2023-08-04 12:43:54 +0300

    Styles the header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import { useState, useEffect } from "react";
 import Editors from "./Editors";
 function App() {
+  const [live, setLive] = useState(true);
   const [html, setHtml] = useState("");
   const [css, setCss] = useState("");
   const [js, setJs] = useState("");
 
   const [srcDoc, setSrcDoc] = useState("");
 
+  const docHandler = () => {
+    setSrcDoc(`
+          <html>
+          <head>
+            <style>${css}</style>
+          </head>
+          <body>
+            ${html}
+            <script>${js}</script>
+          </body>
+        </html>
+          `);
+  };
+
   useEffect(() => {
     const timeout = setTimeout(() => {
-      setSrcDoc(`
+      live &&
+        setSrcDoc(`
       <html>
       <head>
         <style>${css}</style>
@@ -23,7 +39,7 @@ function App() {
     }, 250);
 
     return () => clearTimeout(timeout);
-  }, [html, css, js]);
+  }, [html, css, js, live]);
 
   return (
     <div className="App">
@@ -32,14 +48,36 @@ function App() {
         <Editors language="css" value={css} onChange={setCss} />
         <Editors language="javascript" value={js} onChange={setJs} />
       </div>
-      <div className="container bottom-container">
-        <iframe
-          srcDoc={srcDoc}
-          title="output"
-          sandbox="allow-scripts"
-          width="100%"
-          height="100%"
-        />
+      <div className="middle-container">
+        <div className="middle-container">
+          <button
+            type="button"
+            onClick={() => {
+              setLive((prv) => !prv);
+            }}
+          >
+            Live {live ? "on" : "off"}
+          </button>
+          {!live && (
+            <button
+              type="button"
+              onClick={() => {
+                docHandler();
+              }}
+            >
+              Run
+            </button>
+          )}
+        </div>
+        <div className="container bottom-container">
+          <iframe
+            srcDoc={srcDoc}
+            title="output"
+            sandbox="allow-scripts"
+            width="100%"
+            height="100%"
+          />
+        </div>
       </div>
     </div>
   );
--- src/Editors.js
+++ src/Editors.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUpRightAndDownLeftFromCenter } from "@fortawesome/free-solid-svg-icons";
 import Editor from "@monaco-editor/react";
 
 function Editors({ language, onChange, value }) {
+  const [small, setSmall] = useState(false);
   const handler = (value, event) => {
     onChange(value);
   };
 
   return (
-    <div className="editor">
+    <div className={`editor ${small && "editor-small"}`}>
       <div className="header">
         <h2>{language}</h2>
         <FontAwesomeIcon
           icon={faUpRightAndDownLeftFromCenter}
           className="icon"
+          onClick={() => {
+            setSmall((prv) => !prv);
+          }}
         />
       </div>
       <Editor
